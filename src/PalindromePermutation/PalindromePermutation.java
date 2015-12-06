f6b6df6e4ea6fcfe85283a6884b1919df6cdef5f
vishalshubham <vishal.shubham@gmail.com>
2015-12-05 22:53:42 -0500

    Palindrome Permutation

---
+++ src/PalindromePermutation/PalindromePermutation.java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+public class PalindromePermutation{
+	public static void main(String[] args){
+		Scanner input = new Scanner(System.in);	
+		System.out.println("Enter the string:");
+		String str = input.nextLine();
+		
+		boolean result = palinPerm(str);
+		System.out.println("Is string palindrome? " + result);
+	}
+	
+	public static boolean palinPerm(String str){
+		char[] strArr = str.toCharArray();
+		Arrays.sort(strArr);
+		//System.out.println(strArr);
+			
+		int count = 0;
+		boolean oddCount = false;
+		char temp = ' ';
+		for(int i=0; i< strArr.length; i++){
+			if(temp==' '){
+				temp = strArr[i];
+				count++;
+			}
+			else if(strArr[i] == temp){
+				count++;
+			}
+			else{
+				if((count%2)==1 && oddCount!=true){
+					oddCount = true;
+				}
+				else if((count%2)==1 && oddCount==true){
+					return false;
+				}
+				temp = strArr[i];
+				count = 1;
+			}
+		}
+		if((count%2)==1 && oddCount==true){
+			return false;
+		}
+		return true;
+	}
+}
+
+
