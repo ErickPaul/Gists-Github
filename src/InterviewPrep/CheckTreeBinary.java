b17fab3a9c4c10172f4ae43bcfa6668151b1ead8
vishalshubham <vishal.shubham@gmail.com>
2016-01-19 19:32:10 -0500

    Is binary treecorrected

--- src/InterviewPrep/CheckTreeBinary.java
+++ src/InterviewPrep/CheckTreeBinary.java
@@ -15,9 +15,50 @@ class TreeBinaryNode{
 }
 
 public class CheckTreeBinary{
+
+	public static int min=Integer.MIN_VALUE;
+	
 	public static void main(String[] args){
-		TreeBinaryNode tree = new TreeBinaryNode(12, new TreeBinaryNode(9, new TreeBinaryNode(8), new TreeBinaryNode(10)), new TreeBinaryNode(15, new TreeBinaryNode(14), new TreeBinaryNode(17)));
+		TreeBinaryNode tree = new TreeBinaryNode(12, new TreeBinaryNode(9, new TreeBinaryNode(8), new TreeBinaryNode(10)), new TreeBinaryNode(15, new TreeBinaryNode(11), new TreeBinaryNode(17)));
+		
+		// Wrong method
 		System.out.println(isBinary(tree));
+		// Correct method
+		System.out.println(isBinaryUsingInOrder(tree));
+
+		//inOrder(tree);
+	}
+
+	public static boolean isBinaryUsingInOrder(TreeBinaryNode tree){
+
+		boolean lResult = true;
+		boolean rResult = true;
+
+		if(tree!=null){
+			lResult = isBinaryUsingInOrder(tree.left);
+			if(tree.data<min){
+				return false;
+			}
+			else{
+				min = tree.data;
+			}
+			rResult = isBinaryUsingInOrder(tree.right);
+		}
+
+		if(lResult==false || rResult == false){
+			return false;
+		}
+		else{
+			return true;
+		}
+	}
+
+	public static void inOrder(TreeBinaryNode tree){
+		if(tree!=null){
+			inOrder(tree.left);
+			System.out.println(tree.data);
+			inOrder(tree.right);
+		}
 	}
 
 	public static boolean isBinary(TreeBinaryNode tree){
@@ -44,6 +85,5 @@ public class CheckTreeBinary{
 		else{
 			return true;
 		}
-
 	}
 }
